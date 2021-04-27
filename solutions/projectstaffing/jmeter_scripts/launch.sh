65d3955a85e82644bebfebe6872d405d55ab707f
DenisBPCS <ext-dpitul@bpcs.com>
2021-04-27 21:18:23 +0300

    Adding test_track for SkillsFinder and documentation for it

---
+++ solutions/projectstaffing/jmeter_scripts/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
+s=$(($freeMem/10*8))
+x=$(($freeMem/10*8))
+n=$(($freeMem/10*2))
+export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
+
+echo "JVM_ARGS=${JVM_ARGS}"
+echo "jmeter args=$@"
+
+jmeter $@
