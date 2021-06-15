e62928c78590396fe857c59b664399fb130ec7a7
Denys Serdiuk <dserdiuk@bpcs.com>
2021-06-15 14:32:48 -0700

    correct debug logic

--- solutions/projectstaffing/deployment/arm/scripts/skills_finder_utils/secrets_ops.py
+++ solutions/projectstaffing/deployment/arm/scripts/skills_finder_utils/secrets_ops.py
@@ -6,7 +6,7 @@ from skills_finder_utils.az import az_cli
 
 
 def set_secret(keyvault_name: str, secret_name: str,  value: str):
-    if not value:
+    if value:
         az_cli("keyvault secret set", "--name", secret_name,
                       "--vault-name", keyvault_name, "--value", value)
     else:
