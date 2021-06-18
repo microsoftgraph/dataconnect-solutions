f33d2ff692a940297c96c302883e6980e21c5b5a
Alex Sisu <sisu.eugen@gmail.com>
2021-06-18 16:04:57 +0300

    removed hardcoded values

--- solutions/watercooler/jwc/events-creator/src/main/scala/com/microsoft/graphdataconnect/watercooler/events/creator/management/TeamsMeetingCreator.scala
+++ solutions/watercooler/jwc/events-creator/src/main/scala/com/microsoft/graphdataconnect/watercooler/events/creator/management/TeamsMeetingCreator.scala
@@ -49,38 +49,11 @@ class TeamsMeetingCreator()
     val members: Map[String, Map[String, String]] = objectMapper.readValue(group.group_members, classOf[Map[String, Map[String, String]]])
     members.foreach { member =>
 
-//      if(member._1.contains("darius")) {
         val attendees = new MeetingParticipantInfo
         val identitySet: IdentitySet = new IdentitySet
         val identity: Identity = new Identity
         identity.id = graphService.getUserId(member._1)
         identity.displayName = member._2("name")
-        identitySet.user = identity
-        attendees.identity = identitySet
-        attendeesList.add(attendees)
-//      }
-    }
-
-    attendeesList.addAll(createFakeMeetingAttendeeList())
-    attendeesList
-  }
-
-  private def createFakeMeetingAttendeeList(): util.LinkedList[MeetingParticipantInfo] = {
-    val attendeesList = new util.LinkedList[MeetingParticipantInfo]()
-    List(
-      Tuple2[String, String]("f0e2ad9e-8786-4c78-8a81-e947c58abed6", "andrei@bpcsdevtest.onmicrosoft.com"),
-      Tuple2[String, String]("c0559d7a-c917-4fa8-8457-d7d2baf40451", "alexandru@bpcsdevtest.onmicrosoft.com"),
-      Tuple2[String, String]("0c5abb9f-f045-4864-9ad3-56b77e9ed088", "claudiu@bpcsit.com"),
-      Tuple2[String, String]("05c90ffb-e749-4eee-9e35-4144950c3524", "gary@bpcsit.com"),
-
-      Tuple2[String, String]("56b8c797-745d-42ec-9ac0-6f33fe6f6762", "Claudiu@bpcs.com")
-    ).foreach { case member: Tuple2[String, String] =>
-
-        val attendees = new MeetingParticipantInfo
-        val identitySet: IdentitySet = new IdentitySet
-        val identity: Identity = new Identity
-        identity.id = member._1
-        identity.displayName = member._2
         identitySet.user = identity
         attendees.identity = identitySet
         attendeesList.add(attendees)
