d6164a052b4a96370e23c57065152dda359e5d5b
aborlea <62558039+aborlea@users.noreply.github.com>
2021-07-15 17:55:23 +0000

    Added missing deployment steps (#92)

---
+++ solutions/conversation-lineage/sql/stored_procedure.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION [dbo].[udf_StripHTML] (@HTMLText [VARCHAR](MAX)) RETURNS VARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @Start INT
+    DECLARE @End INT
+    DECLARE @Length INT
+    SET @Start = CHARINDEX('<',@HTMLText)
+    SET @End = CHARINDEX('>',@HTMLText,CHARINDEX('<',@HTMLText))
+    SET @Length = (@End - @Start) + 1
+    WHILE @Start > 0 AND @End > 0 AND @Length > 0
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText,@Start,@Length,'')
+        SET @Start = CHARINDEX('<',@HTMLText)
+        SET @End = CHARINDEX('>',@HTMLText,CHARINDEX('<',@HTMLText))
+        SET @Length = (@End - @Start) + 1
+    END
+    RETURN LTRIM(RTRIM(@HTMLText))
+END
