80d985e32bd7afc79ccd13566868cba5d59eca9d
Denys Serdiuk <dserdiuk@bpcs.com>
2021-07-19 17:46:30 -0700

    improve uninstall.sh to check whether vnet integration needs to be deleted

--- solutions/projectstaffing/deployment/arm/uninstall.sh
+++ solutions/projectstaffing/deployment/arm/uninstall.sh
@@ -113,10 +113,12 @@ if [[ -n "${GDC_M365_SERVICE_SP_NAME}" ]]; then
 
 fi
 
-VNET_INTEGRATION=$(az webapp vnet-integration list -g ${RESOURCE_GROUP} --name ${APP_SERVICE_NAME} --query "[].{Name: name}" -o tsv)
-if [[ -n "${VNET_INTEGRATION}" ]]; then
-  echo "Remove VNET integration for app service ${APP_SERVICE_NAME} from  resource group ${RESOURCE_GROUP}"
-  az webapp vnet-integration remove --name ${APP_SERVICE_NAME} --resource-group ${RESOURCE_GROUP}
+if [[ -n ${APP_SERVICE_NAME} ]]; then
+  VNET_INTEGRATION=$(az webapp vnet-integration list -g ${RESOURCE_GROUP} --name ${APP_SERVICE_NAME} --query "[].{Name: name}" -o tsv)
+  if [[ -n "${VNET_INTEGRATION}" ]]; then
+    echo "Remove VNET integration for app service ${APP_SERVICE_NAME} from  resource group ${RESOURCE_GROUP}"
+    az webapp vnet-integration remove --name ${APP_SERVICE_NAME} --resource-group ${RESOURCE_GROUP}
+  fi
 fi
 
 if [[ -n "${RESOURCE_GROUP}" ]]; then
