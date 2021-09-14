a09bbf71b60d497c49b0cb2a5a3fb2129efdcd54
Zach Basden <87539513+zbasden@users.noreply.github.com>
2021-09-14 15:21:12 -0500

    Add Stored Procedure for backing up landing tables

---
+++ solutions/conversation-lineage-D365/sql/create_sp_crm_backup_table.sql
@@ -0,0 +1,31 @@
+ALTER PROCEDURE dbo.sp_crm_backup_table  @ProcessId INT, @ControlSchemaName NVARCHAR(36), @ControlTableName NVARCHAR(100),
+                @BackupTablePrefix NVARCHAR(36)
+AS 
+
+DECLARE @SinkSchemaName NVARCHAR(36) ;
+DECLARE @SinkTableName NVARCHAR(100);
+DECLARE @sql NVARCHAR(MAX) = '
+SELECT 
+    SinkSchemaName
+    ,SinkTableName
+INTO #SinkTemp
+FROM ' + @ControlSchemaName + '.' + @ControlTableName + ' 
+WHERE ProcessId = ' + convert(varchar(10), @ProcessId)
+
+EXEC sp_executesql @sql
+
+SELECT @SinkSchemaName=[SinkSchemaName]
+        ,@SinkTableName=[SinkTableName]
+FROM #SinkTemp
+
+DECLARE @CreateBackupSQL NVARCHAR(MAX) = '
+IF OBJECT_ID (' + char(39) + @SinkSchemaName + '.' + @BackupTablePrefix + @SinkTableName + char(39) + ') IS NOT NULL 
+BEGIN 
+    DROP TABLE ' + @SinkSchemaName + '.' + @BackupTablePrefix + @SinkTableName + ' 
+END 
+SELECT * 
+INTO '+ @SinkSchemaName + '.' + @BackupTablePrefix + @SinkTableName + ' 
+FROM ' + @SinkSchemaName + '.' + @SinkTableName
+
+EXEC sp_executesql @CreateBackupSQL
+GO;
